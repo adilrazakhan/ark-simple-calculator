9a8140ff8fb5f0685099db057b6ff6f8cf3fa485
adilrazakhan <adil.fxpk@gmail.com>
2024-03-23 03:28:41 +0500

    push my calculator to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ark-simple-calculator" 
---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter firtsNumber", type: "number", name: "firstNumber" },
+    { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+    { message: "select one of the operators t perform opertaion", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponention"], },
+]);
+console.log(answer);
+if (answer.operator === 'Addition') {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === 'Subtraction') {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === 'Multiplication') {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === 'Division') {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === 'Exponention') {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log('Please select a valid operator');
+}
